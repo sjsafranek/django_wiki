efe98043a03929a189fd2a1177398afebbfee2bf
stefan safranek <stefan@stefan.stefan>
2018-09-21 16:02:58 -0700

    fixed login and logout handler

--- login/urls.py
+++ login/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     url(r'^check_permissions', views.checkPermissionsHandler),
 
     # https://simpleisbetterthancomplex.com/tutorial/2016/09/19/how-to-create-password-reset-view.html
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    # url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     auth_views.password_reset_confirm, name='password_reset_confirm'),
+    # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
