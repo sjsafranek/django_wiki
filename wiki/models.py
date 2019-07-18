670456e5c7fafdf94d5b47b785ae0c500db1d816
stefan safranek <stefan@stefan.stefan>
2019-07-18 10:30:05 -0700

    characters

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,5 @@
 import os
-import uuid
+# import uuid
 from django.db import models
 from django.db.models import CharField
 from django.db.models import DateTimeField
--- wikiengine/settings.py
+++ wikiengine/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'login.apps.LoginConfig',
     'uploads.apps.UploadsConfig',
+    'characters.apps.CharactersConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
